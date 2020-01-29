f6f41bbd94c61b1fec8b095ec480f7ff0201b6b0
cubomx <cheservelram657@gmail.com>
2020-01-29 10:12:47 -0600

    Solve both excersises from the golang tour
    Signed-off-by: cubomx <cheservelram657@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	words := make(map[string]int)
+	sentences := strings.Fields(s)
+	for _, val := range sentences {
+		words[val]++ 
+	}
+	return words
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,18 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	var multi = make([][]uint8, dy*3)
+	for i := 0; i < dy; i++ {
+		multi[i] = make([]uint8, dx*3)
+		for j := 0; j < dx; j++ {
+			multi[i][j] = uint8((i+j)/2)
+		}
+	}
+	return multi[:dy]
+}
+
 func main() {
+	pic.Show(Pic)
 }
