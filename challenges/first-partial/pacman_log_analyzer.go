d2f7103de8f82865a6b988ddadb6592fa00b2e65
cubomx <cheservelram657@gmail.com>
2020-02-15 19:02:31 -0600

    Done the first partial challenge
    Signed-off-by: cubomx <cheservelram657@gmail.com>

--- challenges/first-partial/pacman_log_analyzer.go
+++ challenges/first-partial/pacman_log_analyzer.go
@@ -3,7 +3,61 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
+	"io/ioutil"
+	"strings"
+	"strconv"
 )
+
+type data struct {
+	installedDate, lastUpdate, removalDate string
+	manyUpdates int
+}
+
+type generalInfo struct {
+	instPackg, remPackg, upgrPackg, currInstalled int
+}
+
+
+func  generateFile(packages map[string]*data, info *generalInfo, keys []string) {
+	file, err := os.Create("packages_report.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+	systemInfo := "Pacman Packages Report\n----------------------\n"
+	systemInfo += "- Installed packages : " + strconv.Itoa(info.instPackg) + "\n"
+	systemInfo += "- Removed packages   : " + strconv.Itoa(info.remPackg) + "\n"
+	systemInfo += "- Upgraded packages  : " + strconv.Itoa(info.upgrPackg) + "\n"
+	systemInfo += "- Current installed  : " + strconv.Itoa(info.currInstalled) + "\n\n"
+	systemInfo += "List of packages\n----------------\n"
+	_, error := file.WriteString(systemInfo)
+	if error != nil {
+		fmt.Println(err)
+		file.Close()
+		os.Exit(0)
+	}
+	for _, val := range keys{
+		info := packages[val]
+		data := "- Package Name        : " + val + "\n"
+		data += "  - Install date      : " + info.installedDate + "\n"
+		data += "  - Last update date  : " + info.lastUpdate + "\n"
+		data += "  - How many updates  : " + string(info.manyUpdates) + "\n"
+		data += "  - Removal date      : " + info.removalDate + "\n"
+		_, err := file.WriteString(data)
+		if err != nil {
+			fmt.Println(err)
+			file.Close()
+			os.Exit(0)
+		}
+	}
+	fmt.Println("Successful writing")
+	err = file.Close()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+}
 
 func main() {
 	fmt.Println("Pacman Log Analyzer")
@@ -14,5 +68,47 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Your fun starts here.
+	text, err := ioutil.ReadFile(os.Args[1])
+
+	if err != nil {
+		log.Fatal("Unable to read file: %v", err)
+	}
+	lines := strings.Split(string(text), "\n")
+	packages := make(map[string] *data)
+	info := generalInfo{0, 0, 0, 0}
+
+	for _, value := range lines{
+		separated := strings.Fields(value)
+		if len(separated) >= 5{
+			switch separated[3] {
+				case "installed":{
+					date := separated[0][1:] + separated[1][:len(separated[1]) - 1]
+					packages[separated[4]] = &data{date, date, "-", 0}
+					info.instPackg++
+				}
+				case "upgraded" :{
+					date := separated[0][1:] + separated[1][:len(separated[1]) - 1]
+					packages[separated[4]].manyUpdates++
+					packages[separated[4]].lastUpdate = date
+					if packages[separated[4]].manyUpdates == 1 {
+						info.upgrPackg++
+					}
+
+				}
+				case "removed" :{
+					date := separated[0][1:] + separated[1][:len(separated[1]) - 1]
+					packages[separated[4]].removalDate = date
+					info.remPackg++
+				}
+			}
+		}
+	}
+	info.currInstalled = info.instPackg - info.remPackg
+	fmt.Println(info.currInstalled)
+	keys := make([]string, 0, len(packages))
+
+	for key := range packages {
+		keys = append(keys, key)
+	}
+	generateFile(packages, &info, keys)
 }
