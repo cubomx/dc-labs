23fa65f4cad788212b0e0c7c2032b8d6a11c378a
cubomx <cheservelram657@gmail.com>
2020-03-02 12:00:20 -0600

    Already solve the lab
    Signed-off-by: cubomx <cheservelram657@gmail.com>

--- labs/web-crawler/crawl3.go
+++ labs/web-crawler/crawl3.go
@@ -1,26 +1,24 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 241.
-
-// Crawl2 crawls web links starting with the command-line arguments.
-//
-// This version uses a buffered channel as a counting semaphore
-// to limit the number of concurrent calls to links.Extract.
 package main
 
 import (
+	"flag"
 	"fmt"
+	"gopl.io/ch5/links"
 	"log"
 	"os"
-
-	"gopl.io/ch5/links"
+	"time"
 )
 
 //!+sema
 // tokens is a counting semaphore used to
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
+
+type Link struct {
+	depth int
+	used bool
+}
+
 
 func crawl(url string) []string {
 	fmt.Println(url)
@@ -34,31 +32,56 @@ func crawl(url string) []string {
 	return list
 }
 
-//!-sema
+var data = make([] string, 10)
 
-//!+
+func work (link string, actualDepth int, seen map[string]*Link, worklist chan [] string) {
+	go func(link string) {
+		var data = crawl(link)
+		for _, child := range data{
+			seen[child] = &Link{actualDepth + 1, false}
+		}
+		if (actualDepth == 0){
+			fmt.Println(len(data))
+		}
+		worklist <- data
+	}(link)
+}
+
 func main() {
+
 	worklist := make(chan []string)
 	var n int // number of pending sends to worklist
 
 	// Start with the command-line arguments.
 	n++
-	go func() { worklist <- os.Args[1:] }()
-
+	go func() {
+		worklist <- os.Args[1:] }()
+	depth := flag.Int("depth", 1, "limit of depth crawling")
+	flag.Parse()
 	// Crawl the web concurrently.
-	seen := make(map[string]bool)
+	seen := make(map[string]*Link)
 	for ; n > 0; n-- {
 		list := <-worklist
 		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
+			if seen[link] == nil {
+
+				info := &Link{1, true}
+				seen[link] = info
+				fmt.Println("Level:   ", seen[link].depth)
 				n++
-				go func(link string) {
-					worklist <- crawl(link)
-				}(link)
+				work(link, info.depth, seen, worklist)
+
+			} else if !seen[link].used{
+				seen[link].used = true
+				if seen[link].depth <= *depth {
+					fmt.Println("Level:   ", seen[link].depth)
+					n++
+					work(link, seen[link].depth, seen, worklist)
+				}
+			} else {
+				n--
 			}
+			time.Sleep(time.Millisecond * 100)
 		}
 	}
 }
-
-//!-
